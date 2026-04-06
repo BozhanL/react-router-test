361bb398b37a0f1ae653918f435fc7c6c678aeae
BozhanL <BozhanL@users.noreply.github.com>
2026-04-06 08:46:48 +0000

    Deploy preview for PR 1 🛫

---
+++ pr-preview/pr-1/assets/test-CaUPY9hM.js
@@ -0,0 +1,1 @@
+import{D as e,t}from"./jsx-runtime-5s9I2KLv.js";import{t as n}from"./welcome-DDIGLTAb.js";var r=t();function i({}){return[{title:`test`},{name:`description`,content:`Welcome to React Router!`}]}var a=e(function(){return(0,r.jsx)(n,{})});export{a as default,i as meta};
