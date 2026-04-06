2bfdf6b5eea92998cf4b461279c9fbb663ba712e
Bozhan Liang <lbz@rivensbane.com>
2026-04-06 20:51:40 +1200

    Fix test2

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
+import { env } from "process";
 
 export default {
   ssr: false, // disable runtime server rendering
   prerender: true, // pre-render all static routes
+  basename: env.BASE_URL || "/",
 } satisfies Config;
