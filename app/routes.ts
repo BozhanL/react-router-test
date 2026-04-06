c553f3a7ddfe03fbc155ec0a229dd860e5c68bfd
Bozhan Liang <lbz@rivensbane.com>
2026-04-06 20:44:16 +1200

    Test2

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
   index("routes/home.tsx"),
   route("about", "routes/about.tsx"),
   route("test", "routes/test.tsx"),
+  route("test1", "routes/test2.tsx"),
 ] satisfies RouteConfig;
